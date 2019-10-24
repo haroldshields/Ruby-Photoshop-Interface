a3463689e0e530c5bc14d671259ddc6f68f2d14b
haroldshields <haroldshields@mac.com>
2019-10-24 16:56:07 -0700

    Add Windows support

---
+++ sample_photoshop_javascript_for_windows.jsx
@@ -0,0 +1,21 @@
+// sample jsx code.
+
+// do something with the parameters.
+for (var i = 1; i < arguments.length; i++) {
+	// Ignore the 0th argument because it's always undefined as nil as 
+	// it is unshifted to parameters array before it's passed from ruby.
+	// DoJavaScriptFile from the Win32OLE API doesn't work unless the first
+	// item of the parameters arrays is a nil object. 
+	// This seems to be a bug or some sort of undocumented feature.
+	// https://www.adobe.com/content/dam/acom/en/devnet/photoshop/pdfs/photoshop-cc-vbs-ref-2019.pdf
+	alert('argument ' + i + ': ' + arguments[i]);
+}
+
+// test throwing an error.
+if (confirm('Would you like to test throwing an error?')) {
+  var errorString = "message: This error is from Photoshop." + ", line: "+ $.line + ", file: " + File($.fileName).fsName;
+  throw new Error(errorString);
+}
+
+// return a result.
+'This is the result from Photoshop.';
